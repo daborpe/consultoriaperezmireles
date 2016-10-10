6bafb19795b40a7abd84e6041a7b6dbc15d0f96e
Daniel Borjón Pérez <Daniel Borjn Prez>
2016-10-09 20:33:46 -0500

    estilo sitio responsive

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # consultoriaperezmireles
+
+Página acerca del desarrollo de un sitio Web a una consultoría de psicología.
+Para este desarrollo se usará:
+´´´Cool-Kitten Parallax´´´
---
+++ static/js/scripts.js
@@ -0,0 +1,63 @@
+jQuery(document).ready(function ($) {
+
+
+    $(window).stellar();
+
+    var links = $('.navigation').find('li');
+    slide = $('.slide');
+    button = $('.button');
+    mywindow = $(window);
+    htmlbody = $('html,body');
+
+
+    slide.waypoint(function (event, direction) {
+
+        dataslide = $(this).attr('data-slide');
+
+        if (direction === 'down') {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
+        }
+        else {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
+        }
+
+    });
+ 
+    mywindow.scroll(function () {
+        if (mywindow.scrollTop() == 0) {
+            $('.navigation li[data-slide="1"]').addClass('active');
+            $('.navigation li[data-slide="2"]').removeClass('active');
+        }
+    });
+
+    function goToByScroll(dataslide) {
+        htmlbody.animate({
+            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
+        }, 2000, 'easeInOutQuint');
+    }
+
+
+
+    links.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+
+    button.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+
+    });
+
+    
+
+});
+function cambiarTamano(){
+    if($(document).width()==1024){
+        $('#target1').removeClass("grid_3").addClass("grid_12");
+        $('#target2').removeClass().addClass("grid_12");
+        $('#target3').removeClass().addClass("grid_12");
+    }
+}
